e6e1858ed365cb503fa58fd9c61e27e1aa7618f4
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2024-07-29 11:43:07 -0400

    Made NavLink component font-medium

--- components/NavItem.jsx
+++ components/NavItem.jsx
@@ -1,8 +1,10 @@
 export default function NavItem({ name, mobile }) {
   return (
     <div
-      className={`text-black-900 px-4 py-2 transition duration-300 ${
-        mobile ? 'bg-amesRed text-white' : 'hover:bg-amesRed hover:text-white'
+      className={`text-black-900 px-4 py-2 transition duration-300 font-medium  ${
+        mobile
+          ? 'bg-amesRed text-white text-lg'
+          : 'hover:bg-amesRed hover:text-white'
       }`}
     >
       {name}
