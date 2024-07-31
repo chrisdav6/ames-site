3feb662325a1d49fb114b788329925324a0b0fa6
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2024-07-31 14:44:47 -0400

    Added taglines and opacity to HeroCarousel component

--- components/HeroCarousel.jsx
+++ components/HeroCarousel.jsx
@@ -23,7 +23,7 @@ export default function HeroCarousel() {
     >
       {/* <!--Carousel indicators--> */}
       <div
-        className='absolute bottom-0 left-0 right-0 z-[2] mx-[15%] mb-4 flex list-none justify-center p-0'
+        className='absolute bottom-0 left-0 right-0 z-[2] mx-[15%] mb-4 flex list-none justify-center p-0 lg:bottom-20'
         data-twe-carousel-indicators
       >
         <button
@@ -31,24 +31,36 @@ export default function HeroCarousel() {
           data-twe-target='#carouselExampleCaptions'
           data-twe-slide-to='0'
           data-twe-carousel-active
-          className='mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[1000ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none'
+          className='relative mx-[3px] box-content h-[3px] w-[150px] lg:h-[4px] lg:w-[300px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[1000ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none'
           aria-current='true'
           aria-label='Slide 1'
-        ></button>
+        >
+          <span className='absolute top-[-30px] left-[510px] font-base lg:font-bold text-xl text-white'>
+            1
+          </span>
+        </button>
         <button
           type='button'
           data-twe-target='#carouselExampleCaptions'
           data-twe-slide-to='1'
-          className='mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[1000ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none'
+          className='relative mx-[3px] box-content h-[3px] w-[150px] lg:h-[4px] lg:w-[300px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[1000ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none'
           aria-label='Slide 2'
-        ></button>
+        >
+          <span className='absolute top-[-30px] left-[510px] font-base lg:font-bold text-xl text-white'>
+            2
+          </span>
+        </button>
         <button
           type='button'
           data-twe-target='#carouselExampleCaptions'
           data-twe-slide-to='2'
-          className='mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[1000ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none'
+          className='relative mx-[3px] box-content h-[3px] w-[150px] lg:h-[4px] lg:w-[300px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[1000ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none'
           aria-label='Slide 3'
-        ></button>
+        >
+          <span className='absolute top-[-30px] left-[510px] font-base lg:font-bold text-xl text-white'>
+            3
+          </span>
+        </button>
       </div>
 
       {/* <!--Carousel items--> */}
@@ -62,12 +74,26 @@ export default function HeroCarousel() {
         >
           <img
             src='/images/slider-1.jpg'
-            className='block w-full h-[600px] lg:h-[700px] xl:h-[800px] object-cover'
+            className='block w-full h-[600px] lg:h-[700px] xl:h-[800px] object-cover brightness-[60%]'
             alt='...'
           />
-          <div className='absolute inset-x-[15%] bottom-5 hidden py-5 text-center text-white md:block'>
-            <h5 className='text-xl'>First slide label</h5>
-            <p>Some representative placeholder content for the first slide.</p>
+          <div className='absolute inset-x-[15%] top-[25%] md:top-[28%] xl:top-[35%] py-5 text-center text-white md:block'>
+            <h5
+              className='text-5xl lg:text-7xl'
+              style={{
+                textShadow: '0px 0px 10px #000000, 0px 0px 10px #000000',
+              }}
+            >
+              Proven Measurement Products
+            </h5>
+            <p
+              className='text-2xl lg:text-3xl mt-3'
+              style={{
+                textShadow: '0px 0px 10px #000000, 0px 0px 10px #000000',
+              }}
+            >
+              Sensor Technology | Pavement Measurement
+            </p>
           </div>
         </div>
         {/* <!--Second item--> */}
@@ -78,12 +104,26 @@ export default function HeroCarousel() {
         >
           <img
             src='/images/slider-2.jpg'
-            className='block w-full h-[600px] lg:h-[700px] xl:h-[800px] object-cover'
+            className='block w-full h-[600px] lg:h-[700px] xl:h-[800px] object-cover brightness-[60%]'
             alt='...'
           />
-          <div className='absolute inset-x-[15%] bottom-5 hidden py-5 text-center text-white md:block'>
-            <h5 className='text-xl'>Second slide label</h5>
-            <p>Some representative placeholder content for the second slide.</p>
+          <div className='absolute inset-x-[15%] top-[25%] md:top-[28%] xl:top-[35%] py-5 text-center text-white md:block'>
+            <h5
+              className='text-5xl lg:text-7xl'
+              style={{
+                textShadow: '0px 0px 10px #000000, 0px 0px 10px #000000',
+              }}
+            >
+              Dedicated Support
+            </h5>
+            <p
+              className='text-2xl lg:text-3xl mt-3'
+              style={{
+                textShadow: '0px 0px 10px #000000, 0px 0px 10px #000000',
+              }}
+            >
+              We aim to serve our customers with unmatched service
+            </p>
           </div>
         </div>
         {/* <!--Third item--> */}
@@ -94,12 +134,27 @@ export default function HeroCarousel() {
         >
           <img
             src='/images/slider-3.jpg'
-            className='block w-full h-[600px] lg:h-[700px] xl:h-[800px] object-cover'
+            className='block w-full h-[600px] lg:h-[700px] xl:h-[800px] object-cover brightness-[60%]'
             alt='...'
           />
-          <div className='absolute inset-x-[15%] bottom-5 hidden py-5 text-center text-white md:block'>
-            <h5 className='text-xl'>Third slide label</h5>
-            <p>Some representative placeholder content for the third slide.</p>
+          <div className='absolute inset-x-[15%] top-[25%] md:top-[28%] xl:top-[35%] py-5 text-center text-white md:block'>
+            <h5
+              className='text-5xl lg:text-7xl'
+              style={{
+                textShadow: '0px 0px 10px #000000, 0px 0px 10px #000000',
+              }}
+            >
+              Industry Leading Solutions
+            </h5>
+            <p
+              className='text-2xl lg:text-3xl mt-3'
+              style={{
+                textShadow: '0px 0px 10px #000000, 0px 0px 10px #000000',
+              }}
+            >
+              At the forefront in the development of sensor technology for
+              pavement measurements
+            </p>
           </div>
         </div>
       </div>
