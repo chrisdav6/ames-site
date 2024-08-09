f9b2cd0a201c30015631cba54db1dc4db74b3d3f
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2024-08-09 13:24:21 -0400

    Created ProductGridItem component

--- components/ProductGrid.jsx
+++ components/ProductGrid.jsx
@@ -1,85 +1,41 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import ProductGridItem from './ProductGridItem';
 
 export default function ProductGrid() {
   return (
     <section className='grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 w-full px-10 2xl:px-80 py-10'>
-      {/* TODO - Make Individual Product Item Component - pass href, src, text as props */}
-      <Link
+      <ProductGridItem
         href='/products/8300-high-speed-profiler'
-        className='flex flex-col w-full bg-gray-200 group hover:shadow-lg'
-      >
-        <div className='relative w-full h-fit'>
-          <Image
-            src='/images/HighSpeedProfilerProductGrid.jpg'
-            alt='High Speed Inertial Profiler - Model 8300'
-            className='object-cover xl:group-hover:brightness-75 transition duration-300'
-            width={1000}
-            height={1000}
-          />
-        </div>
-        <div className='text-center p-4 xl:group-hover:bg-[#CC0000] xl:group-hover:text-white transition duration-300'>
-          <h2 className='text-xl font-bold'>High Speed Inertial Profiler</h2>
-          <p className='text-lg'>Model 8300</p>
-        </div>
-      </Link>
+        src='/images/HighSpeedProfilerProductGrid.jpg'
+        alt='High Speed Inertial Profiler - Model 8300'
+        title='High Speed Inertial Profiler'
+        subTitle='Model 8300'
+      />
 
-      <Link
+      <ProductGridItem
         href='/products/transverse-profiling-system'
-        className='flex flex-col w-full bg-gray-200 group hover:shadow-lg'
-      >
-        <div className='relative w-full h-fit'>
-          <Image
-            src='/images/TransverseImagingProductGrid.jpg'
-            alt='High Speed Inertial Profiler - Model 8300'
-            className='object-cover xl:group-hover:brightness-75 transition duration-300'
-            width={1000}
-            height={1000}
-          />
-        </div>
-        <div className='text-center p-4 xl:group-hover:bg-[#CC0000] xl:group-hover:text-white transition duration-300'>
-          <h2 className='text-xl font-bold'>Transverse Imaging System</h2>
-          <p className='text-lg'>Rut Depth Measurement</p>
-        </div>
-      </Link>
+        src='/images/TransverseImagingProductGrid.jpg'
+        alt='Transverse Imaging System'
+        title='Transverse Imaging System'
+        subTitle='Rut Depth Measurement'
+      />
 
-      <Link
+      <ProductGridItem
         href='/products/6300-lightweight-profiler'
-        className='flex flex-col w-full bg-gray-200 group hover:shadow-lg'
-      >
-        <div className='relative w-full h-fit'>
-          <Image
-            src='/images/LightweightProfilerProductGrid.jpg'
-            alt='High Speed Inertial Profiler - Model 8300'
-            className='object-cover xl:group-hover:brightness-75 transition duration-300'
-            width={1000}
-            height={1000}
-          />
-        </div>
-        <div className='text-center p-4 xl:group-hover:bg-[#CC0000] xl:group-hover:text-white transition duration-300'>
-          <h2 className='text-xl font-bold'>Lightweight Inertial Profiler</h2>
-          <p className='text-lg'>Model 6300</p>
-        </div>
-      </Link>
+        src='/images/LightweightProfilerProductGrid.jpg'
+        alt='6300 Lightweight Profiler'
+        title='Lightweight Inertial Profiler'
+        subTitle='Model 6300'
+      />
 
-      <Link
+      <ProductGridItem
         href='/products/4200-profilograph'
-        className='flex flex-col w-full bg-gray-200 group hover:shadow-lg'
-      >
-        <div className='relative w-full h-fit'>
-          <Image
-            src='/images/ProfilographProductGrid.jpg'
-            alt='High Speed Inertial Profiler - Model 8300'
-            className='object-cover xl:group-hover:brightness-75 transition duration-300'
-            width={1000}
-            height={1000}
-          />
-        </div>
-        <div className='text-center p-4 xl:group-hover:bg-[#CC0000] xl:group-hover:text-white transition duration-300'>
-          <h2 className='text-xl font-bold'>Profilograph Upgrades</h2>
-          <p className='text-lg'>Model 4200 Tablet Upgrades</p>
-        </div>
-      </Link>
+        src='/images/ProfilographProductGrid.jpg'
+        alt='Profilograph Upgrades'
+        title='Profilograph Upgrades'
+        subTitle='Model 4200 Tablet Upgrades'
+      />
     </section>
   );
 }
---
+++ components/ProductGridItem.jsx
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function ProductGridItem({ href, src, alt, title, subTitle }) {
+  return (
+    <Link
+      href={href}
+      className='flex flex-col w-full bg-gray-200 group hover:shadow-lg'
+    >
+      <div className='relative w-full h-fit'>
+        <Image
+          src={src}
+          alt={alt}
+          className='object-cover xl:group-hover:brightness-75 transition duration-300'
+          width={1000}
+          height={1000}
+        />
+      </div>
+      <div className='text-center p-4 xl:group-hover:bg-[#CC0000] xl:group-hover:text-white transition duration-300'>
+        <h2 className='text-xl font-bold'>{title}</h2>
+        <p className='text-lg'>{subTitle}</p>
+      </div>
+    </Link>
+  );
+}
