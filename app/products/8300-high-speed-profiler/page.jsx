24b18ba044a2dc052eee911e7b184976ac5373db
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2024-09-17 11:59:08 -0400

    Playing with product pages image swiper

--- app/products/8300-high-speed-profiler/page.jsx
+++ app/products/8300-high-speed-profiler/page.jsx
@@ -1,9 +1,29 @@
+import ImageSwiper from '@/components/ImageSwiper';
+
 export const metadata = {
   title: 'Ames Engineering | 8300 High Speed Inertial Road Profiler',
   description:
     'The Ames High Speed Inertial Road Profiler allows for smoothness measurement of a roadway at up to 70 MPH.',
 };
 
+const images = [
+  { src: '/images/AccuTexture100ProductGrid.jpg', alt: 'First' },
+  { src: '/images/SoftwareProductGrid.jpg', alt: 'Second' },
+];
+
 export default function HighSpeedProfiler8300() {
-  return <div>8300 High Speed Inertial Road Profiler</div>;
+  return (
+    <div className='px-10 2xl:px-80 pt-14'>
+      <div>
+        <h3 className='mx-auto md:mx-0 text-2xl font-bold bg-amesRed w-fit py-2 px-3 text-white'>
+          8300 High Speed Inertial Road Profiler
+        </h3>
+      </div>
+
+      {/* Images */}
+      <div className='mt-10'>
+        <ImageSwiper images={images} />
+      </div>
+    </div>
+  );
 }
---
+++ components/ImageSwiper.jsx
@@ -0,0 +1,24 @@
+'use client';
+import Image from 'next/image';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+
+export default function ImageSwiper({ images }) {
+  return (
+    <Swiper className='h-[500px] w-full'>
+      {images.map((image, idx) => (
+        <SwiperSlide key={idx}>
+          <div>
+            <Image
+              src={image.src}
+              width={1000}
+              height={1000}
+              alt={image.alt}
+              className='object-cover'
+            />
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+}
