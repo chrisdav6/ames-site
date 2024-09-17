7ee46c55cccbb7d5340e6e168c582ccd38b46273
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2024-09-17 11:00:01 -0400

    Added all-products page

--- app/all-products/page.jsx
+++ app/all-products/page.jsx
@@ -1,8 +1,20 @@
+import ProductGrid from '@/components/ProductGrid';
+
 export const metadata = {
   title: 'Ames Engineering | Products',
   description: 'See all of our products.',
 };
 
 export default function AllProducts() {
-  return <div>AllProducts</div>;
+  return (
+    <div>
+      <div className='px-10 2xl:px-80 pt-14'>
+        <h3 className='mx-auto md:mx-0 text-2xl font-bold bg-amesRed w-fit py-2 px-3 text-white'>
+          Our Products
+        </h3>
+      </div>
+
+      <ProductGrid />
+    </div>
+  );
 }
