78b1232b2682d88922f19d6b33f75ef4a85c6b6e
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2024-11-20 13:05:23 -0500

    Added Google analytics tag to layout file

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import { GoogleAnalytics } from '@next/third-parties/google';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import { Toaster } from '@/components/ui/toaster';
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
         <main className='flex flex-1 overflow-x-hidden'>{children}</main>
         <Footer />
       </body>
+      <GoogleAnalytics gaId='G-15H2YYSECB' />
     </html>
   );
 }
