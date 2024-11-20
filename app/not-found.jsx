0beb4ca3dd8f0115fc7cf3ee389ee66124c907f5
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2024-11-20 13:33:18 -0500

    Created a custom 404 page

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
 
 export default function Custom404() {
   return (
-    <div className='flex flex-1 items-center justify-center'>NotFound!</div>
+    <div className='flex items-center justify-center w-full px-10 2xl:px-80 pt-14'>
+      <div className='flex flex-col items-center gap-10'>
+        <h1 className='text-3xl text-center'>
+          <span className='font-bold block text-5xl'>Oops!</span> You seem to be
+          on the wrong path!
+        </h1>
+        <Image
+          src='/images/wrongway.png'
+          width={1000}
+          height={1000}
+          alt='404 Not Found!'
+          className='max-w-[350px] rounded-lg shadow-lg'
+        />
+
+        <Link href='/'>
+          <Button className='text-xl'>
+            Let us help you get back on track!
+          </Button>
+        </Link>
+      </div>
+    </div>
   );
 }
