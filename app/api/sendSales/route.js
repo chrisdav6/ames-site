f7a8759557b9077fe5e28fe36deb111a840905cb
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2024-11-26 09:30:20 -0500

    Testing resend with domain on sales request email

--- app/api/sendSales/route.js
+++ app/api/sendSales/route.js
@@ -11,7 +11,8 @@ export async function POST(req) {
 
   try {
     const { data, error } = await resend.emails.send({
-      from: 'Sales Request <onboarding@resend.dev>',
+      // from: 'Sales Request <onboarding@resend.dev>',
+      from: 'Sales Request <website@amesengineering.com>',
       to: ['cdavis@transtechsys.com'],
       subject: 'Ames Sales Request',
       replyTo: email,
