d10aa6dffa6182411e5ce485a605cf8e01bb1d79
Chris Davis <chris.davis5440@gmail.com>
2025-12-03 13:19:28 -0500

    Added holiday closure message to repair form

--- app/support/supportRequest/page.jsx
+++ app/support/supportRequest/page.jsx
@@ -1,4 +1,5 @@
 import { SupportForm } from '@/components/forms/SupportForm';
+import { FaHollyBerry } from 'react-icons/fa';
 
 export const metadata = {
   title: 'Ames Engineering | Support Request',
@@ -20,6 +21,16 @@ export default function SupportRequest() {
           <p className='font-bold'>Email us, we’re happy to help!</p>
         </div>
       </div>
+
+      <p className='mx-auto md:mx-0 bg-amesRed w-fit py-2 px-3 text-white mt-8 text-center md:text-left'>
+        <FaHollyBerry className='inline-flex items-center mr-1' />
+        Please note: We will be closed for the holidays from December 24th and
+        reopening January 2nd. If you are sending in your products
+        <br className='hidden lg:block' />
+        for calibration or service, please make sure they arrive to us before
+        December 23rd to keep them from being held up at the shipping facility.
+        <FaHollyBerry className='inline-flex items-center ml-1' />
+      </p>
 
       <div>
         <SupportForm />
